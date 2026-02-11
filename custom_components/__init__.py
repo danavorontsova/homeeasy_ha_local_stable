36aaec4126b8e60d5bc2cabcf1871c666e0fe3c2
danavorontsova <55919552+danavorontsova@users.noreply.github.com>
2026-02-11 20:28:48 +0200

    Implement custom integration for HVAC control
    Added custom integration for Home Easy HVAC with Home Assistant.

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -1,1 +1,37 @@
-"""Custom components module."""
+"""Custom integration to integrate Home Easy compatible HVAC with Home Assistant."""
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN, PLATFORMS
+from .coordinator import UpdateCoordinator
+
+_LOGGER = logging.getLogger(__package__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up this integration using UI."""
+    if hass.data.get(DOMAIN) is None:
+        hass.data.setdefault(DOMAIN, {})
+
+    ip = entry.data.get("ip")
+    coordinator = UpdateCoordinator(hass, ip)
+    
+    # Перше отримання даних
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    # Реєструємо платформи (наприклад, climate)
+    for platform in PLATFORMS:
+        hass.async_add_job(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
