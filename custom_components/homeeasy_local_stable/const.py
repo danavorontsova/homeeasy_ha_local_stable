0dc718af42578f1129b443dd3a10c01028e9cb39
danavorontsova <55919552+danavorontsova@users.noreply.github.com>
2026-02-11 20:29:38 +0200

    Update constants for Home Easy HVAC integration

--- custom_components/homeeasy_local_stable/const.py
+++ custom_components/homeeasy_local_stable/const.py
@@ -1,11 +1,11 @@
 """Constants for the Home Easy HVAC Local integration"""
 # Base component constants
-NAME = "Home Easy HVAC Local"
-DOMAIN = "homeeasy_local"
+NAME = "Home Easy HVAC STABLE"
+DOMAIN = "homeeasy_local_stable"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.0.0"
+VERSION = "1.2.0"
 ATTRIBUTION = ""
-ISSUE_URL = "https://github.com/ki0ki0/homeeasy_ha_local/issues"
+ISSUE_URL = "https://github.com/danavorontsova/homeeasy_ha_local_stable/issues"
 
 # Icons
 ICON = "mdi:air-conditioner"
@@ -16,17 +16,13 @@ SELECT = "select"
 SWITCH = "switch"
 PLATFORMS = [CLIMATE, SELECT, SWITCH]
 
-
 # Configuration and options
 CONF_IP = "ip"
-
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
 {NAME}
 Version: {VERSION}
 This is a custom integration!
-If you have any issues with this you need to open an issue here:
-{ISSUE_URL}
 -------------------------------------------------------------------
 """
